bba16bc92f9a041c0220cba2c3094153bbaa4c61
Matheus Pinheiro <matheus.pmo@hotmail.com>
2023-10-06 09:46:43 -0300

    Criado funcoes para o Usuario logar no sistema; Criado funcao para o Usuario trocar senha.

--- Model/uUsuario.pas
+++ Model/uUsuario.pas
@@ -13,6 +13,7 @@ type
     FLogin: String;
     FNome: String;
     FPerfilID: Integer;
+    FLogado: Boolean;
   { private declarations }
  protected
   { protected declarations }
@@ -35,7 +36,7 @@ type
 implementation
 
 uses
-  System.SysUtils;
+  System.SysUtils, Vcl.Dialogs;
 
 { TUsuario }
 
@@ -43,18 +44,71 @@ uses
 
 function TUsuario.ChangePassword(strOldPassword,
   strNewPassword: String): Boolean;
+var
+ DBCon:   TDBXConnection;
+ Command: TDBXCommand;
+ T: TDBXTransaction;
 begin
-
+ if FLogado then
+ begin
+  if FSenha = strOldPassword then
+  begin
+   try
+    DBcon        := TDBXConnectionFactory.GetConnectionFactory.GetConnection('DbvendasConnection','SYSDBA','masterkey');
+    T := DBCon.BeginTransaction;
+    Command      := DbCon.CreateCommand;
+    Command.Text := 'UPDATE USUARIOS SET SENHA = ' + QuotedStr(strNewPassword) +
+                             ' WHERE USUARIOID = ' + IntToStr(FID);
+    Command.ExecuteUpdate;
+    DBCon.CommitFreeAndNil(T);
+    Result := True;
+   except
+    DBCon.RollbackFreeAndNil(T);
+   end;
+  end
+  else
+   MessageDlg('A senha antiga não confere',TMsgDlgType.mtInformation,[mbOK],0);
+ end
+ else
+  MessageDlg('É necessário estar logado para invocar este método',TMsgDlgType.mtWarning,[mbOK],0);
 end;
 
 function TUsuario.GetPerfilDescription: String;
+var
+ DBCon:   TDBXConnection;
+ Command: TDBXCommand;
+ Reader:  TDBXReader;
 begin
-
+ if FLogado then
+ begin
+  DBcon        := TDBXConnectionFactory.GetConnectionFactory.GetConnection('DbvendasConnection','SYSDBA','masterkey');
+  Command      := DbCon.CreateCommand;
+  Command.Text := 'SELECT DESCRICAO FROM PERFIL WHERE PERFILID = ' + IntToStr(FPerfilID);
+  Reader := Command.ExecuteQuery;
+  if Reader.Next then
+   Result := Reader.Value[0].GetAnsiString;
+ end
+ else
+  MessageDlg('É necessário estar logado para invocar este método',TMsgDlgType.mtWarning,[mbOK],0);
 end;
 
 function TUsuario.GetPerfilName: String;
+var
+ DBCon:   TDBXConnection;
+ Command: TDBXCommand;
+ Reader:  TDBXReader;
 begin
-
+ if FLogado then
+ begin
+  DBcon        := TDBXConnectionFactory.GetConnectionFactory.GetConnection('DbvendasConnection','SYSDBA','masterkey');
+  Command      := DbCon.CreateCommand;
+  Command.Text := 'SELECT NOME FROM PERFIL WHERE PERFILID = ' + IntToStr(FPerfilID);
+  Reader := Command.ExecuteQuery;
+  if Reader.Next then
+   Result := Reader.Value[0].GetAnsiString;
+ end
+ else
+  MessageDlg('É necessário estar logado para invocar este método',TMsgDlgType.mtWarning,[mbOK],0);
 end;
 
 function TUsuario.LoadProfile: Boolean;
@@ -65,15 +119,25 @@ end;
 function TUsuario.Login(AUsuario, ASenha: String;
   Acoes: TCustomActionList): Boolean;
 var
- DBCon: TDBXConnection;
+ DBCon:   TDBXConnection;
  Command: TDBXCommand;
- Reader: TDBXReader;
+ Reader:  TDBXReader;
 begin
- DBcon := TDBXConnectionFactory.GetConnectionFactory.GetConnection('DbvendasConnection','SYSDBA','masterkey');
- Command  := DbCon.CreateCommand;
+ DBcon        := TDBXConnectionFactory.GetConnectionFactory.GetConnection('DbvendasConnection','SYSDBA','masterkey');
+ Command      := DbCon.CreateCommand;
  Command.Text := 'SELECT * FROM USUARIOS WHERE LOGIN = ' + QuotedStr(AUsuario) +
                                          ' AND SENHA = ' + QuotedStr(ASenha);
  Reader := Command.ExecuteQuery;
+ if Reader.Next then
+  begin
+   FID       := Reader.Value[0].GetInt32;
+   FPerfilID := Reader.Value[1].GetInt32;
+   FNome     := Reader.Value[2].GetAnsiString;
+   FLogin    := Reader.Value[3].GetAnsiString;
+   FSenha    := Reader.Value[4].GetAnsiString;
+   FLogado   := True;
+   Result    := True;
+  end;
 end;
 
 end.
